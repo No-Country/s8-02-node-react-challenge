e186b645d894554134ff467faf9fdf515fb55442
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2023-05-18 21:56:38 -0300

    register terminado

--- mercadoPago/src/pages/register-page/index.jsx
+++ mercadoPago/src/pages/register-page/index.jsx
@@ -1,17 +1,29 @@
 import TopbarLogin from "../../components/login/topbar-login";
 import FormRegister from "../../components/register/form-register";
 import TitleRegister from "../../components/register/Title-register";
+import PopupCorreo from "../../components/register/form-register/popup-register/popupCorreo";
+import { useState } from "react";
 
 
 const RegisterPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handlePopupCorreo = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
   return (
     <section className="bg-[#ECEBF6] w-full h-screen flex flex-col items-center">
       <TopbarLogin />
       <div className="relative">
-       
-        <TitleRegister/>
-        <FormRegister />
+        {isOpen && <PopupCorreo handleClose={handleClose} />}
+
+        <TitleRegister />
+        <FormRegister handlePopupCorreo={handleClick} />
       </div>
     </section>
   );
