5b0d7e69b3a9e23bad98fdb5575d2048f46a1ef7
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2023-05-19 22:39:48 -0300

    areglando conflictos

--- mercadoPago/src/components/register/form-register/index.jsx
+++ mercadoPago/src/components/register/form-register/index.jsx
@@ -1,67 +1,114 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+  AiOutlineInfoCircle,
+} from "react-icons/ai";
+import { useState } from "react";
 
-const FormRegister = () => {
+const FormRegister = ({ handleClick }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="flex  flex-col bg-[#ECEBF6] justify-center px-2  ">
+    <div className="flex  flex-col bg-[#ECEBF6] justify-center px-6  ">
       <div className="bg-white rounded-md shadow-inner  ">
-        <form className="space-y-6 p-8">
-          <div className="group">
-            <label
-              className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
-              htmlFor="email"
-            >
-              Correo electrónico
-            </label>
-            <input
-              id="email"
-              className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-              type="text"
-              placeholder="Correo electrónico"
+        <form className="space-y-6 p-2">
+          <div className="group flex justify-center items-center">
+            <AiOutlineInfoCircle
+              onClick={handleClick}
+              className="w-8 h-8 text-[#6F0B19]"
             />
+            <div className="relative">
+              <label
+                className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
+                htmlFor="email"
+              >
+                Correo electrónico
+              </label>
+              <input
+                id="Correo electrónico"
+                className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
+                type="text"
+                placeholder="Correo electrónico"
+              />
+            </div>
           </div>
-          <div className="group">
-            <label
-              className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
-              htmlFor="dni"
-            >
-              DNI
-            </label>
-            <input
-              id="dni"
-              className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-              type="text"
-              placeholder="DNI"
+          <div className="group flex justify-center items-center">
+            <AiOutlineInfoCircle
+            
+              className="w-6 h-6  text-[#6F0B19]"
             />
+            <div className="relative">
+              <label className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]">
+                DNI
+              </label>
+              <input
+                id="dni"
+                className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
+                type="text"
+                placeholder="DNI"
+              />
+            </div>
           </div>
-          <div className="group">
-            <label
-              className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
-              htmlFor="password"
-            >
-              Contraseña
-            </label>
+          <div className="group flex justify-center items-center ">
+            <AiOutlineInfoCircle className="w-8 h-6   text-[#6F0B19]" />
+            <div className="relative">
+              <label className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]">
+                Contraseña
+              </label>
 
-            <input
-              id="password"
-              className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-              type="password"
-              placeholder="Contraseña"
-            />
+              <input
+                id="contraseña"
+                className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
+                type={showPassword ? "text" : "password"}
+                placeholder="Contraseña"
+              />
+
+              {showPassword ? (
+                <AiOutlineEye
+                  onClick={toggleShowPassword}
+                  className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
+                />
+              ) : (
+                <AiOutlineEyeInvisible
+                  onClick={toggleShowPassword}
+                  className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
+                />
+              )}
+            </div>
           </div>
-          <div className="group">
-            <label
-              className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
-              htmlFor="confirm-pass"
-            >
-              Confirmar contraseña
-            </label>
-            <input
-              id="confirm-pass"
-              className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-              type="password"
-              placeholder="Confirmar contraseña"
-            />
+          <div className="group flex justify-center items-center">
+            <AiOutlineInfoCircle className="w-8 h-8  text-[#6F0B19]" />
+            <div className="relative">
+              <label
+                className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
+                htmlFor="email"
+              >
+                Confirmar contraseña
+              </label>
+              <input
+                id="confirmar contraseña"
+                className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
+                type="password"
+                placeholder="Confirmar contraseña"
+              />
+              {showPassword ? (
+                <AiOutlineEye
+                  onClick={toggleShowPassword}
+                  className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
+                />
+              ) : (
+                <AiOutlineEyeInvisible
+                  onClick={toggleShowPassword}
+                  className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
+                />
+              )}
+            </div>
           </div>
         </form>
       </div>
--- mercadoPago/src/pages/register-page/index.jsx
+++ mercadoPago/src/pages/register-page/index.jsx
@@ -3,15 +3,7 @@ import FormRegister from "../../components/register/form-register";
 import TitleRegister from "../../components/register/Title-register";
 
 const RegisterPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
-    setIsOpen(true);
-  };
-
-  const handleClose = () => {
-    setIsOpen(false);
-  };
 
   return (
     <section className="bg-[#ECEBF6] w-full h-screen flex flex-col items-center">
