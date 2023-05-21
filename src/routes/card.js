a11b4fded5cbe074932969614b5ab916563c8578
Sergio Calbiño <serggio.c86@gmail.com>
2023-05-21 20:57:55 -0300

    validate cards

--- src/routes/card.js
+++ src/routes/card.js
@@ -1,11 +1,32 @@
 import { Router } from "express";
 import { createCard, deleteCard, getCard, updateCard } from "../controllers/card.js";
+import { check } from "express-validator";
+import { idCardValidator, idUserValidator, typeOfCardValidator } from "../middleware/idValidators.js";
+import { validateFields } from "../middleware/validateFields.js";
 
 
 const router=Router()
 
-router.get("/:id",getCard)
-router.post("/:id",createCard)
+router.get("/:id", [
+    check('id', 'It is a not Mongo ID valid'),
+    check('id').custom(idCardValidator),
+    validateFields
+], getCard)
+
+
+router.post("/:id", [
+    check('id', 'It is a not Mongo ID valid'),
+    check('id').custom(idUserValidator),
+    check('type', 'type has invalid characters').not().isEmpty(),
+    check('bank_emisor', 'bank has invalid characters').isString(),
+    check('expiration_date', 'expiration date has invalid characters').isString(),
+    check('user_card', 'user card has invalid characters').isString(),
+    check('user_number', 'user number has invalid characters').isNumeric(),
+    check('cvv', 'cvv has invalid characters').isString(),
+    // check('bank').custom(typeOfCardValidator),
+
+    validateFields
+], createCard)
 router.patch("/:id", updateCard)
 router.delete("/:id",deleteCard)
 
