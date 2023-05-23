da548d36e5f15c9ae53630422837dea2568187df
Galeano Elias <galeanoeliasdavid@gmail.com>
2023-05-23 12:36:41 -0300

    FIX: CONFLICT DEPLOY RESOLVED

--- src/app.js
+++ src/app.js
@@ -48,21 +48,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
- app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*") // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PATCH, DELETE'
-    )
-    next()
-}); 
-
-
 //routes
 app.use("/docs/api/v1", swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/auth", generalRoutes)
