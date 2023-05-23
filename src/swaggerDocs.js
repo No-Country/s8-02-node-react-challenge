f59b114cc72d457baf691f58d9ddcdbd0d90cbf3
Galeano Elias <galeanoeliasdavid@gmail.com>
2023-05-23 19:29:04 -0300

    FIX: SWAGGER

--- src/swaggerDocs.js
+++ src/swaggerDocs.js
@@ -12,7 +12,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://127.0.0.1:5000",
+                url: "https://s8-02-node-react-challenge-api.onrender.com",
                 description: 'API Users for the ...'
             }
         ],
