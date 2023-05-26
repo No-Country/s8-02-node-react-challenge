ef0c96ddab38fe61c482d963943339fb38c84033
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2023-05-26 00:58:25 -0300

    register parejo??

--- mercadoPago/src/components/register/form-register/index.jsx
+++ mercadoPago/src/components/register/form-register/index.jsx
@@ -9,40 +9,54 @@ import { useState } from "react";
 
 const FormRegister = ({ handleClick }) => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+
+  const toggleShowPassword2 = () => {
+    setShowPassword2(!showPassword2);
+  };
 
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
-  const toggleShowPassword1 = () => {
-    setShowPassword(!showPassword);
-  };
 
   return (
-    <form
-    
-    
-      className="flex flex-col items-center justify-center gap-2"
-    >
-      <div className="w-[328px] h-[328px] bg-white rounded-[10px] shadow-cardShadow flex">
-   
-        <div className="flex flex-col  mt-2">
-        <div className="group">
-            <label
-              className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
-              htmlFor="email"
-            >
-              Correo electrónico
-            </label>
-            <input
-              id="email"
-              className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-              type="text"
-              // onChange={handleChange}
-              placeholder="Correo electrónico"
-              name="email"
-            />
+    <form className="flex flex-col items-center justify-center gap-2  ">
+      <div className="w-[328px] h-[328px] bg-white rounded-[10px] shadow-cardShadow flex p-8 pl-4">
+        <div className=" ">
+          <AiOutlineInfoCircle
+            onClick={handleClick}
+            className="w-6 h-6 mt-12     text-[#6F0B19]"
+          />
+          <AiOutlineInfoCircle
+            onClick={handleClick}
+            className="w-6 h-6 mt-8    text-[#6F0B19]"
+          />
+          <AiOutlineInfoCircle
+            onClick={handleClick}
+            className="w-6 h-6 mt-8    text-[#6F0B19]"
+          />
+          <AiOutlineInfoCircle
+            onClick={handleClick}
+            className="w-6 h-6 mt-8    text-[#6F0B19]"
+          />
+        </div>
+        <div className="flex flex-col  mt-2  ">
+          <div className="">
+            <div className="group ">
+              <label className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]">
+                Correo electrónico
+              </label>
+              <input
+                id="email"
+                className="font-medium text-base leading-[16px] placeholder:text-black h-[48px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
+                type="text"
+                // onChange={handleChange}
+                placeholder="Correo electrónico"
+                name="email"
+              />
+            </div>
           </div>
-          
+
           <div className="group">
             <label
               className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
@@ -59,7 +73,7 @@ const FormRegister = ({ handleClick }) => {
               name="dni"
             />
           </div>
-          <div className="group">
+          <div className=" relative group">
             <label
               className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
               htmlFor="email"
@@ -69,10 +83,21 @@ const FormRegister = ({ handleClick }) => {
             <input
               id="contraseña"
               className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-              type="text"
+              type={showPassword2 ? "text" : "password"}
               placeholder="contraseña"
               name="contraseña"
             />
+            {showPassword2 ? (
+              <AiOutlineEye
+                onClick={toggleShowPassword2}
+                className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
+              />
+            ) : (
+              <AiOutlineEyeInvisible
+                onClick={toggleShowPassword2}
+                className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
+              />
+            )}
           </div>
           <div className="relative flex group">
             <div>
@@ -92,12 +117,12 @@ const FormRegister = ({ handleClick }) => {
               {showPassword ? (
                 <AiOutlineEye
                   onClick={toggleShowPassword}
-                  className="absolute w-8 h-8 bottom-2 right-8 group-hover:text-[#39528D]"
+                  className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
                 />
               ) : (
                 <AiOutlineEyeInvisible
                   onClick={toggleShowPassword}
-                  className="absolute w-8 h-8 bottom-2 right-8 group-hover:text-[#39528D]"
+                  className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
                 />
               )}
             </div>
@@ -112,7 +137,7 @@ const FormRegister = ({ handleClick }) => {
       </button>
       <div className="flex items-center justify-center gap-2 mt-2">
         <p className="text-sm font-semibold leading-[17px]">
-        ¿ya tenes cuenta?
+          ¿ya tenes cuenta?
         </p>
         <Link to="/login">
           <p className="text-[#6F0B19] text-sm leading-[17px] font-semibold">
@@ -121,120 +146,7 @@ const FormRegister = ({ handleClick }) => {
         </Link>
       </div>
     </form>
-    // <div className="flex  flex-col bg-[#ECEBF6] justify-center px-6  ">
-    //   <div className="bg-white rounded-md shadow-inner  ">
-    //     <form className="space-y-6 p-2">
-    //       <div className="group flex justify-center items-center">
-    //         <AiOutlineInfoCircle
-    //           onClick={handleClick}
-    //           className="w-8 h-8 text-[#6F0B19]"
-    //         />
-    //         <div className="relative">
-    //           <label
-    //             className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
-    //             htmlFor="email"
-    //           >
-    //             Correo electrónico
-    //           </label>
-    //           <input
-    //             id="Correo electrónico"
-    //             className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-    //             type="text"
-    //             placeholder="Correo electrónico"
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="group flex justify-center items-center">
-    //         <AiOutlineInfoCircle
-
-    //           className="w-6 h-6  text-[#6F0B19]"
-    //         />
-    //         <div className="relative">
-    //           <label className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]">
-    //             DNI
-    //           </label>
-    //           <input
-    //             id="dni"
-    //             className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-    //             type="text"
-    //             placeholder="DNI"
-    //           />
-    //         </div>
-    //       </div>
-    //       <div className="group flex justify-center items-center ">
-    //         <AiOutlineInfoCircle className="w-8 h-6   text-[#6F0B19]" />
-    //         <div className="relative">
-    //           <label className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]">
-    //             Contraseña
-    //           </label>
-
-    //           <input
-    //             id="contraseña"
-    //             className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-    //             type={showPassword ? "text" : "password"}
-    //             placeholder="Contraseña"
-    //           />
-
-    //           {showPassword ? (
-    //             <AiOutlineEye
-    //               onClick={toggleShowPassword}
-    //               className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
-    //             />
-    //           ) : (
-    //             <AiOutlineEyeInvisible
-    //               onClick={toggleShowPassword}
-    //               className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
-    //             />
-    //           )}
-    //         </div>
-    //       </div>
-    //       <div className="group flex justify-center items-center">
-    //         <AiOutlineInfoCircle className="w-8 h-8  text-[#6F0B19]" />
-    //         <div className="relative">
-    //           <label
-    //             className="font-semibold text-xs leading-[15px] pl-2 invisible group-hover:visible group-hover:text-[#39528D]"
-    //             htmlFor="email"
-    //           >
-    //             Confirmar contraseña
-    //           </label>
-    //           <input
-    //             id="confirmar contraseña"
-    //             className="font-medium text-base leading-[16px] placeholder:text-black h-[32px] w-[272px] pl-2 focus:outline-none border-b-2 border-black group-hover:border-[#39528D] placeholder:focus-visible:invisible"
-    //             type="password"
-    //             placeholder="Confirmar contraseña"
-    //           />
-    //           {showPassword ? (
-    //             <AiOutlineEye
-    //               onClick={toggleShowPassword}
-    //               className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
-    //             />
-    //           ) : (
-    //             <AiOutlineEyeInvisible
-    //               onClick={toggleShowPassword}
-    //               className="absolute w-8 h-8 bottom-2 right-2 group-hover:text-[#39528D]"
-    //             />
-    //           )}
-    //         </div>
-    //       </div>
-    //     </form>
-    //   </div>
-    //   <button
-    //     type="submit"
-    //     className="flex w-full mt-4 justify-center rounded-md bg-[#10224D] px-3 py-1.5 text-sm font-semibold leading-6 text-white "
-    //   >
-    //     Crear cuenta
-    //   </button>
-    //   <div className="flex items-center justify-center gap-2 mt-2">
-    //     <p className="text-sm font-semibold leading-[17px]">
-    //       ¿ya tenes cuenta?
-    //     </p>
-    //     <Link to="/login">
-    //       <p className="text-[#6F0B19] text-sm leading-[17px] font-semibold">
-    //         ACCEDER
-    //       </p>
-    //     </Link>
-    //   </div>
-    // </div>
+   
   );
 };
 
