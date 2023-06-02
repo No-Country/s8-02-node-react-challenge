ad20c3aeb4c9f133d10ef3fae4c698d769486258
Einee <yannokaiserfrom@gmail.com>
2023-06-02 16:18:01 -0300

    fix auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -74,7 +74,10 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   let { email, password } = req.body;
   try {
-    let user = await userSchema.findOne({ email });
+    let user = await userSchema.findOne({ email }).populate({
+      path: 'cards',
+      options: { strictPopulate: false }
+    });;
 
     if (!user) {
       return res.status(409).json({ error: "Credenciales errores" });
